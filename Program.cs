5953cdb80e59d0d2b481351f624e1bcd3f98f1ab
colinmc <crmcclintick@gmail.com>
2019-06-15 13:24:30 -0500

    Base program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace TrainExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trains = File.ReadAllLines("trains_6.csv");
+            foreach (string element in trains){
+                System.Console.WriteLine(element);
+            }
+            System.Console.WriteLine(trains[0][0]);
+        }
+    }
+}
